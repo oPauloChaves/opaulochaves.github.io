d973d260c87264f6c93a6d0667d5c3c8ed2fa10d
Paulo Chaves <opaulochaves@gmail.com>
2020-03-28 09:22:08 -0300

    replace jekyll with Gatsby + Contentful

---
+++ gatsby-config.js
@@ -0,0 +1,37 @@
+require('dotenv').config({path: `.env.${process.env.NODE_ENV}`})
+
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  host: process.env.CONTENTFUL_HOST
+}
+
+const { spaceId, accessToken } = contentfulConfig
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    'Contentful spaceId and the access token need to be provided.'
+  )
+}
+
+module.exports = {
+  siteMetadata: {
+    title: 'Paulo Chaves',
+  },
+  pathPrefix: '/',
+  plugins: [
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: ['gatsby-remark-prismjs']
+      }
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: contentfulConfig,
+    }
+  ],
+}
